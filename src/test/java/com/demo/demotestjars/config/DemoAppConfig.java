6f3dfb65cfce5e860c8236b2eaddf04b0ed3af2b
Serhii Barabash <sbarabash@marqeta.com>
2025-05-15 22:34:20 +0300

    overwrite app properties

--- src/test/java/com/demo/demotestjars/config/DemoAppConfig.java
+++ src/test/java/com/demo/demotestjars/config/DemoAppConfig.java
@@ -1,14 +1,10 @@
 package com.demo.demotestjars.config;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.experimental.boot.server.exec.CommonsExecWebServerFactoryBean;
 import org.springframework.experimental.boot.server.exec.MavenClasspathEntry;
+import org.springframework.experimental.boot.server.exec.ResourceClasspathEntry;
 
 @TestConfiguration
 //@EnableDynamicProperty - does not work
@@ -19,29 +15,13 @@ public class DemoAppConfig {
   static CommonsExecWebServerFactoryBean bookShelfServer() {
     return CommonsExecWebServerFactoryBean.builder()
         .useRandomPort(false)
-        .systemProperties((props) -> props.putAll(getBookShelfProperties()))
         .classpath(cp -> cp
             .entries(
                 new MavenClasspathEntry("com.example:bookshelf:0.0.1-SNAPSHOT"),
-                new MavenClasspathEntry("com.h2database:h2:2.3.232")
+                new MavenClasspathEntry("com.h2database:h2:2.3.232"),
+                new ResourceClasspathEntry("book-shelf.properties", "application.properties")
                 )
         );
-  }
-
-  private static Map<String, String> getBookShelfProperties() {
-    try (InputStream input = DemoAppConfig.class.getClassLoader()
-        .getResourceAsStream("book-shelf.properties")) {
-      Properties properties = new Properties();
-      properties.load(input);
-
-      Map<String, String> map = new HashMap<>();
-      for (String key : properties.stringPropertyNames()) {
-        map.put(key, properties.getProperty(key));
-      }
-      return map;
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
   }
 
 }
